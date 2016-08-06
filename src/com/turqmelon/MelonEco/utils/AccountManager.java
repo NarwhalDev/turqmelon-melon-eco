c7184bd570dd1f79ee02c38659139170c9c77fa2
Turqmelon <devonthome@gmail.com>
2016-08-05 21:58:55 -0400

    Fix (pls)

--- src/com/turqmelon/MelonEco/utils/AccountManager.java
+++ src/com/turqmelon/MelonEco/utils/AccountManager.java
@@ -16,28 +16,28 @@ public class AccountManager {
     private static final List<Account> accounts = new ArrayList<>();
     private static List<Currency> currencies = new ArrayList<>();
 
-    public static Currency getDefaultCurrency(){
-        for(Currency currency : getCurrencies()){
-            if (currency.isDefaultCurrency()){
+    public static Currency getDefaultCurrency() {
+        for (Currency currency : getCurrencies()) {
+            if (currency.isDefaultCurrency()) {
                 return currency;
             }
         }
         return null;
     }
 
-    public static Currency getCurrency(String name){
-        for(Currency currency : getCurrencies()){
-            if (currency.getSingular().equalsIgnoreCase(name)||
-                    currency.getPlural().equalsIgnoreCase(name)){
+    public static Currency getCurrency(String name) {
+        for (Currency currency : getCurrencies()) {
+            if (currency.getSingular().equalsIgnoreCase(name) ||
+                    currency.getPlural().equalsIgnoreCase(name)) {
                 return currency;
             }
         }
         return null;
     }
 
-    public static Currency getCurrency(UUID uuid){
-        for(Currency currency : getCurrencies()){
-            if (currency.getUuid().equals(uuid)){
+    public static Currency getCurrency(UUID uuid) {
+        for (Currency currency : getCurrencies()) {
+            if (currency.getUuid().equals(uuid)) {
                 return currency;
             }
         }
@@ -48,30 +48,28 @@ public class AccountManager {
         return currencies;
     }
 
-    public static Account getAccount(Player player){
+    public static Account getAccount(Player player) {
         return getAccount(player.getUniqueId());
     }
 
-    public static Account getAccount(String name){
-        for(Account account : getAccounts()){
-            if (account.getNickname()!=null&&account.getNickname().equalsIgnoreCase(name)){
+    public static Account getAccount(String name) {
+        for (Account account : new ArrayList<>(getAccounts())) {
+            if (account.getNickname() != null && account.getNickname().equalsIgnoreCase(name)) {
                 return account;
             }
         }
         return MelonEco.getDataStore().loadAccount(name);
     }
 
-    public static Account getAccount(UUID uuid){
-        synchronized (accounts) {
-            for (Account account : accounts) {
-                if (uuid == null) return null;
-                if (account.getUuid() == null) continue;
-                if (account.getUuid().equals(uuid)) {
-                    return account;
-                }
+    public static Account getAccount(UUID uuid) {
+        for (Account account : new ArrayList<>(getAccounts())) {
+            if (uuid == null) return null;
+            if (account.getUuid() == null) continue;
+            if (account.getUuid().equals(uuid)) {
+                return account;
             }
-            return MelonEco.getDataStore().loadAccount(uuid);
         }
+        return MelonEco.getDataStore().loadAccount(uuid);
     }
 
     public static List<Account> getAccounts() {

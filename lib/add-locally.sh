f3b508f232dbb5d184980a1070a226848e655442
Ben (OutdatedVersion) <outdated@outdatedversion.com>
2016-10-30 12:22:13 -0500

    switch over to use maven

---
+++ lib/add-locally.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+   -DgroupId=com.archeinteractive \
+   -DartifactId=redis-connect \
+   -Dpackaging=jar \
+   -Dversion=1.0 \
+   -Dfile=redis-connect.jar \
+   -DgeneratePom=true
